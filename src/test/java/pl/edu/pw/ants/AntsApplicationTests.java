6446592adecc6dbdd0d6deaa942dfb813d8b054f
Mateusz Chudek <m.chudek@kaft.pl>
2024-03-07 20:19:40 +0100

    Initial commit

---
+++ src/test/java/pl/edu/pw/ants/AntsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pw.ants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
