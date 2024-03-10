519142b005e881a108507096bf439a6f56b38cfd
Mateusz Chudek <m.chudek@kaft.pl>
2024-03-10 15:45:42 +0100

    Add printRoutes to Solution

--- src/main/java/pl/edu/pw/ants/models/Solution.java
+++ src/main/java/pl/edu/pw/ants/models/Solution.java
@@ -8,4 +8,22 @@ import java.util.List;
 public class Solution {
     private final double cost;
     private final List<List<Node>> routes;
+
+    public void printRoutes(boolean skipDepot) {
+        int routeNumber = 1;
+
+        for (List<Node> route : routes) {
+            System.out.print("Route #" + routeNumber++ + ": ");
+            for (int i = 0; i < route.size(); i++) {
+                // Check if depot should be skipped
+                if (skipDepot && (i == 0 || i == route.size() - 1)) {
+                    continue; // Skip printing the depot node if skipDepot is true
+                }
+                System.out.print(route.get(i).id() + (i < route.size() - 1 ? " " : ""));
+            }
+            System.out.println();
+        }
+
+        System.out.println("Cost " + (int)cost);
+    }
 }
