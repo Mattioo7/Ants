e2f52210ea8b50125f836de9ad028ff2a0fb5a67
Mateusz Chudek <m.chudek@kaft.pl>
2024-03-13 23:51:52 +0100

    Add command line arguments parser

--- src/main/java/pl/edu/pw/ants/AntsCommandLineRunner.java
+++ src/main/java/pl/edu/pw/ants/AntsCommandLineRunner.java
@@ -2,6 +2,11 @@ package pl.edu.pw.ants;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import pl.edu.pw.ants.algorithms.AntColonyOptimization;
@@ -18,29 +23,51 @@ import java.util.List;
 @Slf4j
 public class AntsCommandLineRunner implements CommandLineRunner {
 
-    private final Parser parser;
+    private final Parser problemParser;
 
     @Override
     public void run(String... args) {
         log.info("Starting AntsCommandLineRunner");
-        Problem problem = parser.parse();
-        problem.print();
+
+
+        Options options = new Options();
+        options.addOption("p", "printProblem", false, "Print the problem details");
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = null;
+
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.err.println("Error parsing command line arguments: " + e.getMessage());
+            System.exit(1);
+        }
+
+        Problem problem = problemParser.parse();
+
+        if (cmd.hasOption("printProblem")) {
+            problem.print();
+        }
 
         boolean skipDepot = false;
         List<List<Node>> routes;
 
-        System.out.println("\nRunning greedy algorithm");
-        CvrpAlgorithm greedyAlgorithm = new GreedyAlgorithm(problem);
-        routes = greedyAlgorithm.solve();
-        greedyAlgorithm.printRoutes(routes, skipDepot);
-
-        System.out.println("\nRunning ant colony optimization algorithm");
-        CvrpAlgorithm antColonyOptimization = new AntColonyOptimization(problem, 10, 1.0, 2.0, 0.5, 10.0, 1.0, 5000);
-        routes = antColonyOptimization.solve();
-        if (routes == null) {
-            System.out.println("No solution found");
-            return;
+        for (int i = 0; i < 1; i++) {
+            System.out.println("\nRunning greedy algorithm");
+            CvrpAlgorithm greedyAlgorithm = new GreedyAlgorithm(problem);
+            routes = greedyAlgorithm.solve();
+            greedyAlgorithm.printRoutes(routes, skipDepot);
         }
-        antColonyOptimization.printRoutes(routes, skipDepot);
+
+        for (int i = 0; i < 1; i++) {
+            System.out.println("\nRunning ant colony optimization algorithm");
+            CvrpAlgorithm antColonyOptimization = new AntColonyOptimization(problem, 10, 1.0, 2.0, 0.5, 10.0, 1.0, 5000);
+            routes = antColonyOptimization.solve();
+            if (routes == null) {
+                System.out.println("No solution found");
+                return;
+            }
+            antColonyOptimization.printRoutes(routes, skipDepot);
+        }
     }
 }
